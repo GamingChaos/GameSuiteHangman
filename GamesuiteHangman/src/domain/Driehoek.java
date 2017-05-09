7cdbddb36119d7c3b3e07eaad35b5e2159d01657
Bram Vandendaele <bramvandendaele1@gmail.com>
2017-05-09 13:20:24 +0200

    driehoek: typer: Bram denker: Bas

--- GamesuiteHangman/src/domain/Driehoek.java
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -9,11 +9,14 @@ public class Driehoek {
 	}
 	private void setHoekPunten(Punt punt1, Punt punt2, Punt punt3) {
 		if(punt1 == null || punt2 == null || punt3 == null) throw new DomainException("punten mogen niet null zijn");
+		if(punt1.equals(punt2) || punt1.equals(punt3) || punt2.equals(punt3)) throw new DomainException("punten mogen niet hetzelfde zijn!");
+		if((punt2.getX() - punt1.getX())*(punt3.getY() - punt1.getY()) == (punt3.getX() - punt1.getX())*(punt2.getY() - punt1.getY())) throw new DomainException("Punten mogen niet op een lijn liggen");
 		this.punt1 = punt1;
 		this.punt2 = punt2;
 		this.punt3 = punt3;
 		
 	}
+	
 	public Punt getHoekPunt1(){
 		return punt1;
 	}
@@ -23,5 +26,19 @@ public class Driehoek {
 	}
 	public Punt getHoekPunt3(){
 		return punt3;
+	}
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof Driehoek){
+			Driehoek d = (Driehoek) o;
+			if(getHoekPunt1() == d.getHoekPunt1() || getHoekPunt1() == d.getHoekPunt2() || getHoekPunt1() == d.getHoekPunt3() && getHoekPunt2() == d.getHoekPunt2()|| getHoekPunt2() == d.getHoekPunt1() || getHoekPunt2() == d.getHoekPunt3() && getHoekPunt3() == d.getHoekPunt3() || getHoekPunt3() == d.getHoekPunt1() || getHoekPunt3() == d.getHoekPunt2()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	@Override
+	public String toString(){
+		return "Driehoek: hoekpunt1: " + getHoekPunt1().toString() + " - hoekpunt2: " + getHoekPunt2().toString() + " - hoekpunt3: " + getHoekPunt3().toString(); 
 	}
 }
