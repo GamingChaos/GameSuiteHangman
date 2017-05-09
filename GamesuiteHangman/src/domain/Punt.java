1ad382adac08af14553cfc4a46d537602985512a
Bram Vandendaele <bramvandendaele1@gmail.com>
2017-05-09 11:05:03 +0200

    Punt v1

--- GamesuiteHangman/src/domain/Punt.java
+++ GamesuiteHangman/src/domain/Punt.java
@@ -1,5 +1,35 @@
 package domain;
 
 public class Punt {
-
+int x = 0;
+int y = 0;
+	public Punt(int x, int y ){
+		this.setX(x);
+		this.setY(y);
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof Punt){
+			Punt p = (Punt) o;
+			if(this.getX() == p.getX() && this.getY() == p.getY()) return true;
+			
+		}
+		return false;
+	}
+	@Override
+	public String toString(){
+		return "(" + getX() + ", " + getY() + ")";
+	}
 }
