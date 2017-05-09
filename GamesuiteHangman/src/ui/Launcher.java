cc81587410f58672b0c6d634be31429bc5fff2ee
Bram Vandendaele <bramvandendaele1@gmail.com>
2017-05-09 11:12:59 +0200

    launcher v2

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -2,15 +2,26 @@ package ui;
 
 import javax.swing.JOptionPane;
 
+import domain.Punt;
 import domain.Speler;
 
 public class Launcher {
 
 	public static void main(String[] args) {
+		try{
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler = new Speler(naam);
+		
+		int x = Integer.parseInt(JOptionPane.showInputDialog("x coordinaat van het punt:"));
+		int y = Integer.parseInt(JOptionPane.showInputDialog("y coordinaat van het punt:"));
+		Punt p = new Punt(x, y);
+		JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + p.toString());
 
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
