b7461f03f1e897e0716aa4bf72f6b98c7006e23a
GamingChaos <vincent.stuyck@student.ucll.be>
2017-05-09 13:04:25 +0200

    no message

--- GamesuiteHangman/src/domain/DomainException.java
+++ GamesuiteHangman/src/domain/DomainException.java
@@ -4,4 +4,8 @@ public class DomainException extends RuntimeException{
 	public DomainException(Exception exception){
 		super(exception);
 	}
+	
+	public DomainException(String message){
+		super(message);
+	}
 }
