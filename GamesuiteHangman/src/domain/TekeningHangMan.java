bd61b67f2d3d938025f4a1bc52b01fc23bfc2c35
Bram Vandendaele <bramvandendaele1@gmail.com>
2017-05-11 15:10:07 +0200

    TekeningHangmanTest en TekeningHangMan gemaakt

--- GamesuiteHangman/src/domain/TekeningHangMan.java
+++ GamesuiteHangman/src/domain/TekeningHangMan.java
@@ -1,7 +1,66 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TekeningHangMan {
-public TekeningHangMan(){
-	
-}
+	private List<Vorm> vormen = new ArrayList<Vorm>();
+
+	public TekeningHangMan() {
+		vormen = new ArrayList<Vorm>();
+
+	}
+
+	public void voegVormToe(Vorm vorm) {
+		if (vorm == null)
+			throw new DomainException("vorm moet bestaan");
+		vormen.add(vorm);
+
+	}
+
+	public List getLijst() {
+		return vormen;
+	}
+
+	public int getGrootte() {
+		return vormen.size();
+	}
+
+	public List getAantalZichtbare() {
+		List zichtbare = new ArrayList<Vorm>();
+		for (Vorm vorm : vormen) {
+			if (vorm.isZichtbaar())
+				zichtbare.add(vorm);
+		}
+		return zichtbare;
+	}
+
+	public void zetVolgendeZichtbaar() {
+		if (getAantalZichtbare().size() == vormen.size())
+			throw new DomainException("Alles is al zichtbaar");
+		for (Vorm vorm : vormen) {
+			if (vorm.isZichtbaar() == false) {
+				vorm.setZichtbaarheid(true);
+				break;
+			}
+		}
+
+	}
+
+	public Vorm getVorm(int index){
+		return vormen.get(index);
+	}
+
+	public void reset() {
+		for (int i = 3; i < vormen.size(); i++) {
+			if (vormen.get(i).isZichtbaar() == true) {
+				vormen.get(i).setZichtbaarheid(false);
+			}
+		}
+
+	}
+	public void verwijder(Vorm vorm){
+		if(!vormen.contains(vorm)) throw new DomainException("Vorm zit niet in de lijst");
+		vormen.remove(vorm);
+	}
 }
