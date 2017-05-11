a36cb18ac3cb444a56992224fa09ad2de760adce
GamingChaos <vincent.stuyck@student.ucll.be>
2017-05-11 15:42:12 +0200

    hangman word getekend

--- GamesuiteHangman/src/domain/Tekening.java
+++ GamesuiteHangman/src/domain/Tekening.java
@@ -22,8 +22,46 @@ public class Tekening implements Drawable{
 	private String naam;
 	private List<Vorm> vormen = new ArrayList<Vorm>();
 	private static final int MIN_X = 0,  MIN_Y = 0, MAX_X = 399, MAX_Y = 399;
+	
 	public Tekening(String naam){
 		this.setNaam(naam);
+		
+		Vorm galgBodem = new Rechthoek(new Punt(10, 350), 300, 40);
+		vormen.add(galgBodem);
+		Vorm galgstaaf = new LijnStuk(new Punt(160, 350), new Punt(160, 50));
+		vormen.add(galgstaaf);
+		Vorm hangbar = new LijnStuk(new Punt(160, 50), new Punt(280, 50));
+		vormen.add(hangbar);
+		Vorm koord = new LijnStuk(new Punt(280, 50), new Punt(280, 100));
+		vormen.add(koord);
+		Vorm hoofd = new Cirkel(new Punt(280, 125), 25);
+		vormen.add(hoofd);
+		Vorm oogLinks = new Cirkel(new Punt(270, 118), 2);
+		vormen.add(oogLinks);
+		Vorm oogRechts = new Cirkel(new Punt(290, 118), 2);
+		vormen.add(oogRechts);
+		Vorm neus = new Cirkel(new Punt(280, 128), 2);
+		vormen.add(neus);
+		Vorm mond = new LijnStuk(new Punt(270, 138), new Punt(290, 138));
+		vormen.add(mond);
+		Vorm lijf = new LijnStuk(new Punt(280, 150), new Punt(280, 250));
+		vormen.add(lijf);
+		Vorm beenLinks = new LijnStuk(new Punt(280, 250), new Punt(240, 310));
+		vormen.add(beenLinks);
+		Vorm beenRechts = new LijnStuk(new Punt(280, 250), new Punt(320, 310));
+		vormen.add(beenRechts);
+		Vorm voetLinks = new Cirkel(new Punt(240, 310), 5);
+		vormen.add(voetLinks);
+		Vorm voetRechts = new Cirkel(new Punt(320, 310), 5);
+		vormen.add(voetRechts);
+		Vorm armLinks = new LijnStuk(new Punt(280, 200), new Punt(230, 170));
+		vormen.add(armLinks);
+		Vorm armRechts = new LijnStuk(new Punt(280, 200), new Punt(330, 170));
+		vormen.add(armRechts);
+		Vorm handLinks = new Cirkel(new Punt(230, 170), 5);
+		vormen.add(handLinks);
+		Vorm handRechts = new Cirkel(new Punt(330, 170), 5);
+		vormen.add(handRechts);
 	}
 	public String getNaam(){
 		return naam;
@@ -36,6 +74,9 @@ public class Tekening implements Drawable{
 	}
 	public Vorm getVorm(int index){
 		return vormen.get(index);
+	}
+	public List<Vorm> getVormLijst(){
+		return vormen;
 	}
 	public int getAantalVormen(){
 		return vormen.size();
