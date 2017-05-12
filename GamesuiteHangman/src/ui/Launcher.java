f35f03eeeabc1938423b31d9a274faf4eb585cd8
DESKTOP-S9TUGJM\kenzo <coenaerts.kenzo@gmail.com>
2017-05-12 09:52:53 +0200

    test

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -40,7 +40,7 @@ public class Launcher {
 			String spel = (String) JOptionPane.showInputDialog(null, "Welk spel wil je spelen?", "Super Awesome Mega Game", JOptionPane.QUESTION_MESSAGE, null, games, games[0]);
 			switch(spel){
 			case "HangMan":
-				HangManUi hangui = new HangManUi(speler);
+				HangManUI hangui = new HangManUI(speler);
 				break;
 			case "Pictionary":
 				PictionaryUI picui = new PictionaryUI(speler);
